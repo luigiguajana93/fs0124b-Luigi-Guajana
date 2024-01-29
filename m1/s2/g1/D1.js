4f1a9962043aeffb885fc945c1cd5adc9e852b8f
luigiguajana93 <luigi.guajana@gmail.com>
2024-01-29 18:04:38 +0100

    modificato

--- m1/s2/g1/D1.js
+++ m1/s2/g1/D1.js
@@ -82,8 +82,4 @@ console.log (d-x);
 
 let name1 = "john";
 let name2 = "John" ;
-console.log (name1 == name2);
-
-name2 = name2.toLowerCase()
-
-console.log(name1 == name2);
+console.log (name1.toLowerCase() === name2.toLowerCase())
