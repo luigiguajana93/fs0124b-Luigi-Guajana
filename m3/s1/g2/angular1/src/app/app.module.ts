1047ecd2aa2cf76a4611145fb83646d6bedd265d
luigiguajana93 <luigi.guajana@gmail.com>
2024-03-19 15:00:17 +0100

    esercizio angular

---
+++ m3/s1/g2/angular1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Component1Component } from './components/component1/component1.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Component3Component } from './components/component3/component3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
