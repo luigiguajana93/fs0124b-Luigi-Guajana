bcd40dc4d5631d990e3d0b93c75e1b0d9ac50ee7
luigiguajana93 <luigi.guajana@gmail.com>
2024-04-16 16:39:10 +0200

    consegna finalone

--- backend/m1/s1/g1/untitled/src/Main.java
+++ backend/m1/s1/g1/untitled/src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int risultato = moltiplicazione(10, 5);
-        int concat = concatenazione ( 5, 'ciao');
+        int concat = concatenazione ( 5, "anni");
         System.out.println("il risultato della moltiplicazione è " + risultato);
+        System.out.println("il risultato della concatenazione è " + concat);
     }
 
     public static int moltiplicazione (int a , int b){
